293344006a1a34697f1a370367c3e3147a2a2b9b
TokyManambina <75074136+TokyManambina@users.noreply.github.com>
2020-12-04 07:07:10 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing",{
+    strings: ["Developer","Gamer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true
+});
+
+var typed1 = new Typed(".typing1",{
+    strings: ["Developer","Gamer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true
+});
